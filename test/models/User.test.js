546b5ca319653c96755185aa96611da7e1be159e
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-05-01 17:02:12 -0500

    Agregando los setters de username y bio

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -34,4 +34,19 @@ describe('Unit tests for User class', () => {
 		expect(cristianoRonaldo.getDateCreated).not.toBeUndefined();
 		expect(cristianoRonaldo.getLastUpdated).not.toBeUndefined();
 	});
+
+	test('3) Add setters', () => {
+		const cristianoRonaldo = new User(
+			1,
+			'CR7',
+			'Cristiano Ronaldo',
+			'Futbolista profesional del Manchester United'
+		);
+
+		cristianoRonaldo.setUsername = 'ElComandante';
+		expect(cristianoRonaldo.username).toBe('ElComandante');
+
+		cristianoRonaldo.setBio = 'La nueva Bio';
+		expect(cristianoRonaldo.bio).toBe('La nueva Bio');
+	});
 });
