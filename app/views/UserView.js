109b233ff1351345b494df2dd701ebc1ec7ff3db
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-05-02 23:18:46 -0500

    Agregando validación para enviar un error al recibir un valor null

---
+++ app/views/UserView.js
@@ -0,0 +1,11 @@
+const User = require('./../models/User');
+
+class UserView {
+	static createUser(payload) {
+		if (payload === null) {
+			return { error: 'El payload no existe' };
+		}
+	}
+}
+
+module.exports = UserView;
